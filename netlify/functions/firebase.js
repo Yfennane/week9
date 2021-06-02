398dcf53add91f0fc3c72ce23599e4fdc291b72c
Your Name <younes.fennane@kellogg.northwestern.edu>
2021-06-02 10:09:07 -0500

    WIP-Younes

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyCZBasD4sAErq6POuBJUmT_nXXToZb-W6U",
+  authDomain: "kiei-451-12785.firebaseapp.com",
+  projectId: "kiei-451-12785",
+  storageBucket: "kiei-451-12785.appspot.com",
+  messagingSenderId: "524818386110",
+  appId: "1:524818386110:web:9221265991c744aa4ba2ef"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
